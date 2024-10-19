642c00c66853a432c543d3972cd5ba5ffd521df7
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-10-19 22:19:44 +0500

    Update heapsort.c
    A bit more efficient and convenient...

--- heapsort.c
+++ heapsort.c
@@ -4,8 +4,13 @@ void main()
 {
     int heap[10], no, i, j, c, root, temp;
  
-    printf("\n Enter no of elements :");
-    scanf("%d", &no);
+printf("\nEnter number of elements (max 10): ");
+scanf("%d", &no);
+if (no > 10) 
+{ 
+    printf("Number of elements exceeds the maximum limit of 10.\n");
+    return 1; // Exit if the number of elements exceeds the limit
+}
     printf("\n Enter the nos : ");
     for (i = 0; i < no; i++)
        scanf("%d", &heap[i]);
@@ -37,9 +42,17 @@ void main()
         do 
         {
             c = 2 * root + 1;    /* left node of root element */
-            if ((heap[c] < heap[c + 1]) && c < j-1)
-                c++;
-            if (heap[root]<heap[c] && c<j)    /* again rearrange to max heap array */
+    if (c < j) 
+    { // Check if left child is within bounds
+    if (c + 1 < j && heap[c] < heap[c + 1]) // Check if right child exists and is greater
+        c++; // Move to the right child
+    if (heap[root] < heap[c]) { // Swap if root is less than the largest child
+        temp = heap[root];
+        heap[root] = heap[c];
+        heap[c] = temp;
+    }
+    root = c; // Move down to the child
+}
             {
                 temp = heap[root];
                 heap[root] = heap[c];
@@ -48,7 +61,7 @@ void main()
             root = c;
         } while (c < j);
     } 
-    printf("\n The sorted array is : ");
+    printf("\n The sorted array is: ");
     for (i = 0; i < no; i++)
        printf("\t %d", heap[i]);
     printf("\n Complexity : \n Best case = Avg case = Worst case = O(n logn) \n");
